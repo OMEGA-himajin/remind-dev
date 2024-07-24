007182186208e97342c0a7653ad6a3de1d5df347
ganondorofu <ganondorofu3143@outlook.com>
2024-07-24 17:26:33 +0900

    commit

---
+++ ios/remind-dev/remind_devApp.swift
@@ -0,0 +1,17 @@
+//
+//  remind_devApp.swift
+//  remind-dev
+//
+//  Created by STEMASK on 2024/07/24.
+//
+
+import SwiftUI
+
+@main
+struct remind_devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
