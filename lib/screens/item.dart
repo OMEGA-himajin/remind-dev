fabc5885c71c677ebbc0e56108e98c9165294652
OMEGA <lightsound2321@gmail.com>
2024-08-31 10:36:29 +0900

    Revert "fix: 通知を受け取ったときの処理条件分岐させるの忘れてた"
    This reverts commit e803f72915becbb4010bf60d11863fc337785c4f.

--- lib/screens/item.dart
+++ lib/screens/item.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import '../model/firestore_items.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
@@ -16,9 +17,9 @@ class _ItemsScreenState extends State<ItemsScreen> {
   Map<Item, Set<int>> itemSelections = {};
   List<ScanResult> scanResults = [];
   bool isConnected = false;
-  final List<Item> _items = [];
-  final ItemRepository _itemRepository = ItemRepository();
-  final uid = main.uid;
+  final List<Item> _items = []; // 全アイテムのリスト
+  final ItemRepository _itemRepository = ItemRepository(); // アイテムリポジトリのインスタンス
+  final uid = main.uid; // UIDを適切に設定
   BluetoothDevice? connectedDevice;
   BluetoothCharacteristic? targetCharacteristic;
 
@@ -43,37 +44,42 @@ class _ItemsScreenState extends State<ItemsScreen> {
     super.initState();
     startScan();
     _searchController.addListener(_filterItems);
-    _filteredItems = List.from(_items);
-    _getItems();
+    _filteredItems = List.from(_items); // 初期状態では全アイテムを表示
+    _getItems(); // アイテムを取得するメソッドを呼び出す
   }
 
   void _addItem(String tagId) {
-    setState(() {
-      _filteredItems.add(Item(name: '名前未設定', tagId: tagId, inBag: false));
-    });
+    if (mounted) {
+      setState(() {
+        _filteredItems.add(Item(name: '名前未設定', tagId: tagId, inBag: false));
+      });
+    }
   }
 
   void _onSelectionChanged(Item item, Set<int> set, String newTagId) {
-    setState(() {
-      item.inBag = set.contains(1);
-    });
-    _itemRepository.updateItemDetails(
-      uid,
-      item.tagId,
-      newTagId,
-      item.name,
-      item.inBag,
-    );
-    item.tagId = newTagId;
+    if (mounted) {
+      setState(() {
+        item.inBag = set.contains(1);
+      });
+      _itemRepository.updateItemDetails(
+        uid, // UID
+        item.tagId, // 既存のtagId
+        newTagId, // 新しいtagId（変更がない場合）
+        item.name,
+        item.inBag,
+      );
+      item.tagId = newTagId;
+    }
   }
 
   Future<void> _getItems() async {
     List<Item> items = (await _itemRepository.getAllItems(uid)).cast<Item>();
-    setState(() {
-      _items.clear();
-      _items.addAll(items);
-      _filteredItems = List.from(_items);
-    });
+    if (mounted) {
+      setState(() {
+        _items.addAll(items);
+        _filteredItems = List.from(_items);
+      });
+    }
     print(items);
   }
 
@@ -93,8 +99,8 @@ class _ItemsScreenState extends State<ItemsScreen> {
       setState(() {
         connectedDevice = device;
         isConnected = true;
+        Navigator.of(context).pop();
       });
-      Navigator.of(context).pop();
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Connected to ${device.name}')),
       );
@@ -110,17 +116,17 @@ class _ItemsScreenState extends State<ItemsScreen> {
     List<Item> items = await _itemRepository.getAllItems(uid);
     Item? existingItem = items.firstWhere(
       (item) => item.tagId == tagId,
-      orElse: () => Item(tagId: '', name: ''),
+      orElse: () => Item(tagId: '', name: ''), // 一致するアイテムがない場合のダミーアイテム
     );
 
     if (existingItem.tagId.isNotEmpty) {
+      // アイテムが存在する場合、inBagをtrueに更新
       await _itemRepository.updateItemDetails(
           uid, tagId, tagId, existingItem.name, true);
-      // Navigator.pushReplacement(
-      //     context,
-      //     MaterialPageRoute(
-      //         builder: (BuildContext context) => main._getAppBarTitle(3)));
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) () main._getAppBar(3)));
     } else {
+      // アイテムが存在しない場合、新しいアイテムを追加
       Item newItem = Item(name: '名前未設定', tagId: tagId, inBag: true);
       await _itemRepository.addItem(uid, newItem);
       setState(() {
@@ -246,7 +252,7 @@ class _ItemsScreenState extends State<ItemsScreen> {
       builder: (BuildContext context) {
         return AlertDialog(
           title: const Text('削除確認'),
-          content: Text('${item.name} を削除しますか？'),
+          content: Text('$item を削除しますか？'),
           actions: [
             TextButton(
               onPressed: () {
@@ -299,135 +305,120 @@ class _ItemsScreenState extends State<ItemsScreen> {
           ],
         ),
       ),
-      body: FutureBuilder<List<Item>>(
-        future: _itemRepository.getAllItems(uid),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No items found'));
-          } else {
-            _items.clear();
-            _items.addAll(snapshot.data!);
-            _filteredItems = List.from(_items);
-            return ListView.builder(
-              itemCount: _filteredItems.length,
-              itemBuilder: (context, index) {
-                final item = _filteredItems[index];
-                return ListTile(
-                  title: Text(item.name),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SegmentedButton<int>(
-                        showSelectedIcon: false,
-                        onSelectionChanged: (Set<int> set) =>
-                            _onSelectionChanged(item, set, item.tagId),
-                        segments: [
-                          ButtonSegment(
-                              value: 1,
-                              label: Icon(Icons.backpack,
-                                  color: Colors.grey[700])),
-                          ButtonSegment(
-                              value: 0,
-                              label: Icon(Icons.no_backpack,
-                                  color: Colors.grey[700])),
-                        ],
-                        selected: item.inBag ? {1} : {0},
-                      ),
-                      IconButton(
-                        icon: const Icon(
-                          Icons.edit,
-                          color: Colors.grey,
-                        ),
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              TextEditingController nameController =
-                                  TextEditingController(text: item.name);
-                              TextEditingController tagIdController =
-                                  TextEditingController(text: item.tagId);
-
-                              return AlertDialog(
-                                title: const Text('編集'),
-                                content: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    TextField(
-                                      controller: nameController,
-                                    ),
-                                    const Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Text('アイテム名'),
-                                    ),
-                                    TextField(
-                                      controller: tagIdController,
-                                    ),
-                                    const Align(
-                                      alignment: Alignment.centerLeft,
-                                      child: Text('タグID'),
-                                    ),
-                                    SizedBox(
-                                      width: 200,
-                                      child: ElevatedButton(
-                                        onPressed: () {
-                                          _showDeleteDialog(item);
-                                        },
-                                        child: const Text(
-                                          '削除',
-                                          style: TextStyle(color: Colors.red),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text('キャンセル'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () async {
-                                      await _itemRepository.updateItemDetails(
-                                        uid,
-                                        _filteredItems[index].tagId,
-                                        tagIdController.text,
-                                        nameController.text,
-                                        _filteredItems[index].inBag,
-                                      );
-                                      Navigator.of(context).pop();
-                                      setState(() {
-                                        _filteredItems[index] = Item(
-                                          tagId: tagIdController.text,
-                                          name: nameController.text,
-                                          inBag: _filteredItems[index].inBag,
-                                        );
-                                      });
-                                    },
-                                    child: const Text('保存'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                      ),
-                    ],
+      body: ListView.builder(
+        itemCount: _filteredItems.length,
+        itemBuilder: (context, index) {
+          final item = _filteredItems[index]; // ここで item を定義
+          return ListTile(
+            title: Text(item.name),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SegmentedButton<int>(
+                  showSelectedIcon: false,
+                  onSelectionChanged: (Set<int> set) =>
+                      _onSelectionChanged(item, set, item.tagId),
+                  segments: [
+                    ButtonSegment(
+                        value: 1,
+                        label: Icon(Icons.backpack, color: Colors.grey[700])),
+                    ButtonSegment(
+                        value: 0,
+                        label:
+                            Icon(Icons.no_backpack, color: Colors.grey[700])),
+                  ],
+                  selected: item.inBag ? {1} : {0},
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.edit,
+                    color: Colors.grey,
                   ),
-                  onTap: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('${item.name} tapped')),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        TextEditingController nameController =
+                            TextEditingController(text: item.name);
+                        TextEditingController tagIdController =
+                            TextEditingController(text: item.tagId);
+
+                        return AlertDialog(
+                          title: const Text('編集'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextField(
+                                controller: nameController,
+                              ),
+                              const Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text('アイテム名'),
+                              ),
+                              TextField(
+                                controller: tagIdController,
+                              ),
+                              const Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text('タグID'),
+                              ),
+                              SizedBox(
+                                width: 200,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    _showDeleteDialog(item);
+                                  },
+                                  child: const Text(
+                                    '削除',
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('キャンセル'),
+                            ),
+                            TextButton(
+                              onPressed: () async {
+                                // Firestoreの値を更新
+                                await _itemRepository.updateItemDetails(
+                                  uid, // UID
+                                  _filteredItems[index].tagId, // 既存のtagId
+                                  tagIdController.text, // 新しいtagId
+                                  nameController.text,
+                                  _filteredItems[index].inBag,
+                                );
+                                Navigator.of(context).pop();
+                                setState(() {
+                                  _filteredItems[index] = Item(
+                                    tagId: tagIdController.text,
+                                    name: nameController.text,
+                                    inBag: _filteredItems[index].inBag,
+                                  );
+                                });
+                              },
+                              child: const Text('保存'),
+                            ),
+                          ],
+                        );
+                      },
                     );
                   },
-                );
-              },
-            );
-          }
+                ),
+              ],
+            ),
+            onTap: () {
+              // アイテムがタップされたときの処理
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('$item tapped')),
+              );
+            },
+          );
         },
       ),
     );
